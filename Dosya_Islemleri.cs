90d8a1c30d3e909ced89269b76df8a96f4d56668
DOG.US YALCIN <dogu_s123@hotmail.com>
2019-04-03 17:15:39 +0300

    dogus commit deneme

---
+++ Dosya_Islemleri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dosyadaki_hakikat
+{
+    class Dosya_Islemleri
+    {
+
+
+
+
+
+
+        public static void Dosyaoku(string doc_path)
+        {
+            FileStream fileStream = new FileStream(doc_path, FileMode.Open, FileAccess.Read);
+            StreamReader swstream = new StreamReader(fileStream);
+
+        }
+    }
+}
