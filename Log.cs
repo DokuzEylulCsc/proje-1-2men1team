80246055d7bd87eec0a81028761d4ed6b720a607
jvoljvolizka <jvoljvolizka@gmail.com>
2019-04-06 20:05:56 -0700

    log start

---
+++ Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dosyadaki_hakikat
+{
+    class Log
+    {
+        void newlog(string stuff)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace dosyadaki_hakikat
             string[] sinesfromfile;
             
 
-            //test meretleri silmeyi unutma
+           
             try
             {
                 sinesfromfile = Dosya_Islemleri.Read_File(args[0]);
@@ -21,11 +21,11 @@ namespace dosyadaki_hakikat
 
             Dosya_Islemleri.sendtosort(Dosya_Islemleri.findstuff(sinesfromfile));
             InSort sorter = new InSort(Dosya_Islemleri.Tamlist, Dosya_Islemleri.Noktalilist);
-            //  sorter(Dosya_Islemleri.Noktalilist);
+           
             sorter.getsorted();
             Console.ReadLine();
 
-            // test meretleri son
+           
         }
     }
 }
